d3e89cead4b28ac1dafdf2b9377499976dd0c06f
Jethro Hazelhurst <j.hazelhurst1994@gmail.com>
2020-01-11 15:10:07 +0000

    Add methods to CafesController

--- app/Http/Controllers/API/CafesController.php
+++ app/Http/Controllers/API/CafesController.php
@@ -7,19 +7,46 @@ use Illuminate\Http\Request;
 
 class CafesController extends Controller
 {
-    /**
-     *
-     */
+
+    /*
+    |-------------------------------------------------------------------------------
+    | Get All Cafes
+    |-------------------------------------------------------------------------------
+    | URL:            /api/v1/cafes
+    | Method:         GET
+    | Description:    Gets all of the cafes in the application
+    */
     public function getCafes()
     {
 
     }
 
-    /**
-     * 
-     */
-    public function getCafe()
+    /*
+    |-------------------------------------------------------------------------------
+    | Get An Individual Cafe
+    |-------------------------------------------------------------------------------
+    | URL:            /api/v1/cafes/{id}
+    | Method:         GET
+    | Description:    Gets an individual cafe
+    | Parameters:
+    |   $id   -> ID of the cafe we are retrieving
+    */
+    public function getCafe($id)
     {
 
     }
+
+    /*
+    |-------------------------------------------------------------------------------
+    | Adds a New Cafe
+    |-------------------------------------------------------------------------------
+    | URL:            /api/v1/cafes
+    | Method:         POST
+    | Description:    Adds a new cafe to the application
+    */
+    public function postNewCafe(
+    {
+
+    }
+
 }
