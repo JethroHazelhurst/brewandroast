9cc939b00ed549e435a27c2b7ed7decf6b47b5e1
Jethro Hazelhurst <j.hazelhurst1994@gmail.com>
2020-01-11 21:02:27 +0000

    Configure vuex with vue

---
+++ resources/js/store.js
@@ -0,0 +1,19 @@
+/**
+ * Import vue and vuex.
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+/**
+ * Initialise vuex with vue.
+ */
+Vue.use( Vuex )
+
+/**
+ * Export our dtat store.
+ */
+export default new Vuex.Store({
+    modules: {
+
+    }
+});
