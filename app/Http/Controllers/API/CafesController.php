3debb9d5fecbe923116d47a61f33c22a7e7e099b
Jethro Hazelhurst <j.hazelhurst1994@gmail.com>
2020-01-14 18:59:29 +0000

    Add the GoogleMaps utility to the Cafes Controller

--- app/Http/Controllers/API/CafesController.php
+++ app/Http/Controllers/API/CafesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Utilities\GoogleMaps;
 use App\Http\Requests\StoreCafeRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -51,13 +52,22 @@ class CafesController extends Controller
     */
     public function postNewCafe(StoreCafeRequest $request)
     {
+        $coordinates = GoogleMaps::geocodeAddress(
+            $request->get('address'),
+            $request->get('city'),
+            $request->get('state'),
+            $request->get('zip')
+        );
+
         $cafe = new Cafe();
 
-        $cafe->name = $request->get('name');
-        $cafe->address = $request->get('address');
-        $cafe->city = $request->get('city');
-        $cafe->state = $request->get('state');
-        $cafe->zip = $request->get('zip');
+        $cafe->name      = $request->get('name');
+        $cafe->address   = $request->get('address');
+        $cafe->city      = $request->get('city');
+        $cafe->state     = $request->get('state');
+        $cafe->zip       = $request->get('zip');
+        $cafe->latitude  = $request->get('lat');
+        $cafe->longitude = $request->get('lng');
 
         $cafe->save();
 
