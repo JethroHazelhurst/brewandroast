b5bcc2c2bde47b1f9d55db3562b9114fe2e4344e
Jethro Hazelhurst <j.hazelhurst1994@gmail.com>
2020-01-23 15:46:59 +0000

    Add user likes cafe relationship to the models

--- app/Models/Cafe.php
+++ app/Models/Cafe.php
@@ -11,22 +11,32 @@ class Cafe extends Model
     /**
      *
      */
-    public function children(){
+    public function children()
+    {
         return $this->hasMany('App\Models\Cafe', 'parent', 'id');
-
     }
 
     /**
      *
      */
-    public function parent(){
+    public function parent()
+    {
         return $this->hasOne('App\Models\Cafe', 'id', 'parent');
     }
 
     /**
      *
      */
-    public function brewMethods(){
+    public function brewMethods()
+    {
         return $this->belongsToMany( 'App\Models\BrewMethod', 'cafes_brew_methods', 'cafe_id', 'brew_method_id' );
+    }
+
+    /**
+     *
+     */
+    public function likes()
+    {
+        return $this->belongsToMany( 'App\Models\User', 'users_cafes_likes', 'cafe_id', 'user_id');
     }
 }
