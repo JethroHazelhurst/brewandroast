70d74e0b564a503e016e1006bc3d6bae463b0997
Jethro Hazelhurst <j.hazelhurst1994@gmail.com>
2020-01-25 20:23:10 +0000

    Add the cafe-tag relationship

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable = [
+        'name'
+    ];
+
+    public function cafes(){
+        return $this->belongsToMany( 'App\Models\Cafe', 'cafes_users_tags', 'tag_id', 'user_id');
+    }
 }
