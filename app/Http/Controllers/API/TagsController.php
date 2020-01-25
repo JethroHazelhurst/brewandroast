dbc3e0f5814461a7ea9d5309499e18fef9ddce19
Jethro Hazelhurst <j.hazelhurst1994@gmail.com>
2020-01-25 22:48:21 +0000

    Flesh out the tags controller

--- app/Http/Controllers/API/TagsController.php
+++ app/Http/Controllers/API/TagsController.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
+use App\Models\Tag;
+use Request;
 
 class TagsController extends Controller
 {
