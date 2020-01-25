da4c956829a0eab8338ebeb05ab59cb19bf92438
Jethro Hazelhurst <j.hazelhurst1994@gmail.com>
2020-01-25 22:57:23 +0000

    Add get where like tag query

--- app/Http/Controllers/API/TagsController.php
+++ app/Http/Controllers/API/TagsController.php
@@ -8,5 +8,26 @@ use Request;
 
 class TagsController extends Controller
 {
-    //
+
+    /**
+     * THIS IS A SIMPLE GET WHERE LIKE QUERY
+     */
+    public function getTags(){
+        $query = Request::get('search');
+
+        /*
+         * If the query is not set or is empty, load all the tags.
+         * Otherwise load the tags that match the query
+         */
+        if ($query == null || $query == '') {
+            $tags = Tag::all();
+        } else {
+            $tags = Tag::where('tag', 'LIKE', $query.'%')->get();
+        }
+
+        /*
+         * Return all of the tags as JSON
+         */
+        return response()->json( $tags );
+    }
 }
